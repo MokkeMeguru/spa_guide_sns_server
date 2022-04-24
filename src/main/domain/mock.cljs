68c14d7cfd5e41c973081621b2c23ef9c0619426
MokkeMeguru <meguru.mokke@gmail.com>
2022-04-25 02:35:40 +0900

    fix time

--- src/main/domain/mock.cljs
+++ src/main/domain/mock.cljs
@@ -2,6 +2,11 @@
   (:require [domain.community]
             [domain.community.event]))
 
+(defn ->UnixTime [year month day hour minute second]
+  (let [dt (js/Date. (js/Date.UTC year (dec month) day hour minute second))]
+    (.setHours dt (- (.getHours dt) 9))
+    (.getTime dt)))
+
 (def samples
   {:user
    [{:id "6e803bdf-55a7-4a31-849e-8489cc76a457"
@@ -212,7 +217,7 @@
      :owned-member-id  "08da804a-8ec9-494c-bd28-f67218e30851"
      :name "研修を打ち上げていく会"
      :details "辛さを調整できるお手軽カレー屋でパーッといこう"
-     :hold-at (.getTime (js/Date. (js/Date.UTC 2022 5 2 18 30 0)))
+     :hold-at (->UnixTime 2022 5 2 18 30 0)
      :category :party
      :image-url  (str domain.community.event/dummy-image-base-url "/id/249/{width}/{height}.jpg")}
     {:id "d6889952-b566-4ac0-a201-5be14fca97d9"
@@ -220,7 +225,7 @@
      :owned-member-id "08da804a-8ec9-494c-bd28-f67218e30851"
      :name "中華とワインを楽しめるおすすめレストラン"
      :details "合うんです"
-     :hold-at (.getTime (js/Date. (js/Date.UTC 2022 5 6 18 30 0)))
+     :hold-at (->UnixTime 2022 5 6 18 30 0)
      :category :party
      :image-url (str domain.community.event/dummy-image-base-url "/id/249/{width}/{height}.jpg")}
     {:id "9a0570db-0c1a-4558-8ac2-505147431d29"
@@ -228,7 +233,7 @@
      :owned-member-id "eb86ddc9-6446-44d3-8afa-5def58bbe340"
      :name "片手間に食べれるお手軽カレーパンを買いに行く"
      :details "なんやて！？"
-     :hold-at (.getTime (js/Date. (js/Date.UTC 2022 5 11 13 30 0)))
+     :hold-at (->UnixTime 2022 5 11 13 30 0)
      :category :party
      :image-url  (str domain.community.event/dummy-image-base-url "/id/452/{width}/{height}.jpg")}
     {:id "1790d7f0-b4fc-4440-b476-8e2b953ef506"
@@ -236,7 +241,7 @@
      :owned-member-id "eb86ddc9-6446-44d3-8afa-5def58bbe340"
      :name "激辛ラーメンを求め、レビュー平均5点なあの店へ"
      :details "情報を食いに行く"
-     :hold-at (.getTime (js/Date. (js/Date.UTC 2022 5 12 12 30 0)))
+     :hold-at (->UnixTime 2022 5 12 12 30 0)
      :category :party
      :image-url (str domain.community.event/dummy-image-base-url "/id/139/{width}/{height}.jpg")}
     {:id "b6ee37fe-9191-417d-a932-f2d3d0e307dd"
@@ -244,7 +249,7 @@
      :owned-member-id "eb86ddc9-6446-44d3-8afa-5def58bbe340"
      :name "社食の胡椒を使い切る会"
      :details "社食の胡椒を増やすべく、まずは需要を \"わからせ\" ていく会"
-     :hold-at (.getTime (js/Date. (js/Date.UTC 2022 5 16 12 0 0)))
+     :hold-at (->UnixTime 2022 5 16 12 0 0)
      :category :party
      :image-url (str domain.community.event/dummy-image-base-url "/id/139/{width}/{height}.jpg")}
     {:id "98ebcf3a-2f88-4205-aa69-ce6d9590ab3c"
@@ -252,7 +257,7 @@
      :owned-member-id "eb86ddc9-6446-44d3-8afa-5def58bbe340"
      :name "辛ラメーンを教会で食べた話"
      :details "大学時代ボッチ飯キメていたら、知らない先輩と教会に行って辛ラーメンを食べさせてもらった話"
-     :hold-at (.getTime (js/Date. (js/Date.UTC 2022 5 17 13 0 0)))
+     :hold-at (->UnixTime 2022 5 17 13 0 0)
      :category :seminar
      :image-url (str domain.community.event/dummy-image-base-url "/id/452/{width}/{height}.jpg")}
     {:id "8c340942-15cf-40e2-8568-44f336639adc"
@@ -260,7 +265,7 @@
      :owned-member-id "eb86ddc9-6446-44d3-8afa-5def58bbe340"
      :name "ペペロンチーノのコツ"
      :details "茹で汁とオリーブオイルの効率的な扱い方を解説"
-     :hold-at (.getTime (js/Date. (js/Date.UTC 2022 5 27 18 0 0)))
+     :hold-at (->UnixTime 2022 5 27 18 0 0)
      :category :seminar
      :image-url (str domain.community.event/dummy-image-base-url "/id/452/{width}/{height}.jpg")}
     {:id "a849c8ae-1519-4119-b646-d0a846fdc94d"
@@ -268,7 +273,7 @@
      :owned-member-id "2f7c3359-9d4f-4873-b281-67a46cb8ae1e"
      :name "今夜もう一度来てください、本当のエビチリを食べさせてあげますよ"
      :details "素材からこだわったスペシャルなディナー、作ります"
-     :hold-at (.getTime (js/Date. (js/Date.UTC 2022 5 29 19 30 0)))
+     :hold-at (->UnixTime 2022 5 29 19 30 0)
      :category :party
      :image-url (str domain.community.event/dummy-image-base-url "/id/249/{width}/{height}.jpg")}
     {:id "399370c4-2187-45a9-bc76-c348f518fda9"
@@ -276,7 +281,7 @@
      :owned-member-id "2f7c3359-9d4f-4873-b281-67a46cb8ae1e"
      :name "夏バテに効くおすすめお家辛味レシピ20選"
      :details "俺の好きなカレー知ってる？ナスカレー"
-     :hold-at (.getTime (js/Date. (js/Date.UTC 2022 5 30 18 00 0)))
+     :hold-at (->UnixTime 2022 5 30 18 00 0)
      :category :party
      :image-url (str domain.community.event/dummy-image-base-url "/id/452/{width}/{height}.jpg")}
     {:id "383503d8-dea4-4f26-85b5-afe00f29184b"
@@ -284,7 +289,7 @@
      :owned-member-id "08da804a-8ec9-494c-bd28-f67218e30851"
      :name "会社近くの美味しい韓国料理店を周る集まり"
      :details "会社近くにある辛味の聞いた美味しいチーズタッカルビを出すお店を見つけたので行きましょうの会"
-     :hold-at (.getTime (js/Date. (js/Date.UTC 2022 6 3 19 30 0)))
+     :hold-at (->UnixTime 2022 6 3 19 30 0)
      :category :party
      :image-url (str domain.community.event/dummy-image-base-url "/id/249/{width}/{height}.jpg")}
     ;; スイートなスイーツ
@@ -293,7 +298,7 @@
      :owned-member-id "0500f01e-47cd-4753-bc5d-74716b9b32a5"
      :name "コンビニで買える、ンまぁ〜いプリンを布教したい"
      :details "プリンなんてチャンチャラおかしくて…"
-     :hold-at (.getTime (js/Date. (js/Date.UTC 2022 5 5 15 00 0)))
+     :hold-at (->UnixTime 2022 5 5 15 00 0)
      :category :party
      :image-url (str domain.community.event/dummy-image-base-url "/id/452/{width}/{height}.jpg")}
     {:id "6b63c4f3-3725-48c5-b9aa-738ed7e7e03f"
@@ -301,7 +306,7 @@
      :owned-member-id  "7b0497e0-c6ba-4fde-8788-2c3abf3d241f"
      :name "港区アフタヌーンティーを体験しにいく会"
      :details "上流階級の嗜みを体験すべく、我々はアマゾンの奥地へと向かった"
-     :hold-at (.getTime (js/Date. (js/Date.UTC 2022 5 7 11 00 0)))
+     :hold-at (->UnixTime 2022 5 7 11 00 0)
      :category :party
      :image-url (str domain.community.event/dummy-image-base-url "/id/452/{width}/{height}.jpg")}
     ;; 肉体改造部
@@ -310,7 +315,7 @@
      :owned-member-id "a93f7653-0cd9-498a-bf5b-8822d81047c5"
      :name "お家でできる有酸素運動"
      :details "お手軽でお財布に優しい健康促進メソッド"
-     :hold-at (.getTime (js/Date. (js/Date.UTC 2022 5 13 19 30 0)))
+     :hold-at (->UnixTime 2022 5 13 19 30 0)
      :category :seminar
      :image-url (str domain.community.event/dummy-image-base-url "/id/1084/{width}/{height}.jpg")}
     ;; モン○トの集い
@@ -320,7 +325,7 @@
      :owned-member-id "06e45e1b-f801-47b9-9a28-485786aa85d6"
      :name "Clojureとかいう動的型付け言語に対して Golang の圧倒的優位性を見出していく会"
      :details "静的型付け言語で圧倒的安全性と可用性を見せていけ"
-     :hold-at (.getTime (js/Date. (js/Date.UTC 2022 5 3 13 0 0)))
+     :hold-at (->UnixTime 2022 5 3 13 0 0)
      :category :seminar
      :image-url (str domain.community.event/dummy-image-base-url "/id/593/{width}/{height}.jpg")}
     {:id  "14cd0825-04ee-4f27-a2d5-05bda29fb98c"
@@ -328,7 +333,7 @@
      :owned-member-id "06e45e1b-f801-47b9-9a28-485786aa85d6"
      :name "Python で研究して Ruby を研修した僕が 3日で Golang を身に着けた話"
      :details "インタプリタ駆動開発の体験で柔らかくなった脳をコンパイル駆動開発で引き締めよう"
-     :hold-at (.getTime (js/Date. (js/Date.UTC 2022 5 4 15 0 0)))
+     :hold-at (->UnixTime 2022 5 4 15 0 0)
      :category :seminar
      :image-url (str domain.community.event/dummy-image-base-url "/id/1082/{width}/{height}.jpg")}
     {:id "c4d1a191-6d2a-425f-826c-391370c823da"
@@ -336,7 +341,7 @@
      :owned-member-id "06e45e1b-f801-47b9-9a28-485786aa85d6"
      :name "GitHub Actions を Golang で書き直す会"
      :details "あの有名 Actions も静的型付け言語でリプレース"
-     :hold-at (.getTime (js/Date. (js/Date.UTC 2022 5 5 10 0 0)))
+     :hold-at (->UnixTime 2022 5 5 10 0 0)
      :category :party
      :image-url (str domain.community.event/dummy-image-base-url "/id/1078/{width}/{height}.jpg")}
     ;; 健全なる精神は健全なる身体に宿る
@@ -345,7 +350,7 @@
      :owned-member-id "10a2167e-7c6e-46d2-8964-0e969e01d2a4"
      :name "肩こりをほぐしながらメンタルアップ"
      :details "デスクワークで固まったのは体ですか？頭ですか？"
-     :hold-at (.getTime (js/Date. (js/Date.UTC 2022 5 9 12 15 0)))
+     :hold-at (->UnixTime 2022 5 9 12 15 0)
      :category :seminar
      :image-url (str domain.community.event/dummy-image-base-url "/id/844/{width}/{height}.jpg")}
     ;; 有酸素運動クラブ
@@ -354,7 +359,7 @@
      :owned-member-id "d9013a2a-ff40-4392-9fe5-99a39e71b101"
      :name "仕事終わりの一発をキメる会"
      :details "サンドバックパンチ！サンドバックパンチ！"
-     :hold-at (.getTime (js/Date. (js/Date.UTC 2022 5 10 19 15 0)))
+     :hold-at (->UnixTime 2022 5 10 19 15 0)
      :category :party
      :image-url (str domain.community.event/dummy-image-base-url "/id/844/{width}/{height}.jpg")}
     ;; 組み合わせ最適化から始める競技プログラミング
@@ -363,7 +368,7 @@
      :owned-member-id "00262340-a5db-4607-b90d-49223eb38730"
      :name "深さ優先探索の基礎とテンプレ"
      :details "初級の壁の一つ、DFSの典型問題と、回答を加速させるテンプレートを紹介"
-     :hold-at (.getTime (js/Date. (js/Date.UTC 2022 5 6 15 0 0)))
+     :hold-at (->UnixTime 2022 5 6 15 0 0)
      :category :seminar
      :image-url (str domain.community.event/dummy-image-base-url "/id/896/{width}/{height}.jpg")}
     {:id "8a6966fc-4ba5-485c-9358-3a7a279e92b4"
@@ -371,7 +376,7 @@
      :owned-member-id "3b247c12-ed77-4aa3-accc-67c034743af5"
      :name "競プロで1秒でも長く考えるための環境設定"
      :details "自動テスト、スニペットなどのお役立ち情報をシェアする会"
-     :hold-at (.getTime (js/Date. (js/Date.UTC 2022 5 6 18 0 0)))
+     :hold-at (->UnixTime 2022 5 6 18 0 0)
      :category :seminar
      :image-url (str domain.community.event/dummy-image-base-url "/id/1082/{width}/{height}.jpg")}
     ;; エンジニアが神絵師になるまで
@@ -380,7 +385,7 @@
      :owned-member-id "5dd531bf-e78e-49e6-9929-44c236bfce9a"
      :name "仕事とプライベートを切り分けるベストプラクティス"
      :details "デュアルブートのすゝめ"
-     :hold-at (.getTime (js/Date. (js/Date.UTC 2022 5 6 15 30 0)))
+     :hold-at (->UnixTime 2022 5 6 15 30 0)
      :category :seminar
      :image-url (str domain.community.event/dummy-image-base-url "/id/998/{width}/{height}.jpg")}
     {:id "e50bf396-f31c-4177-8b76-25e2b5315648"
@@ -388,7 +393,7 @@
      :owned-member-id "5dd531bf-e78e-49e6-9929-44c236bfce9a"
      :name "あなたの描き筋はどっち？"
      :details "自分にあった描き方で快適に描く"
-     :hold-at (.getTime (js/Date. (js/Date.UTC 2022 5 11 15 30 0)))
+     :hold-at (->UnixTime 2022 5 11 15 30 0)
      :category :seminar
      :image-url (str domain.community.event/dummy-image-base-url "/id/1082/{width}/{height}.jpg")}
     {:id "417916ab-132a-426f-8a66-dd53b4113fbb"
@@ -396,7 +401,7 @@
      :owned-member-id "5dd531bf-e78e-49e6-9929-44c236bfce9a"
      :name "正しい線の描き方"
      :details "地道な反復練習から確実なステップアップを目指す会"
-     :hold-at (.getTime (js/Date. (js/Date.UTC 2022 5 13 15 30 0)))
+     :hold-at (->UnixTime 2022 5 13 15 30 0)
      :category :seminar
      :image-url (str domain.community.event/dummy-image-base-url "/id/896/{width}/{height}.jpg")}
     ;; Ruby on Rails は MVP へ通ず
@@ -405,7 +410,7 @@
      :owned-member-id "7cad0e2d-0858-40c3-906b-6e75eded06ee"
      :name "Rails の教典の最新版、Ruby on Rails チュートリアルを読む"
      :details "Rails を信じよ"
-     :hold-at (.getTime (js/Date. (js/Date.UTC 2022 6 1 17 0 0)))
+     :hold-at (->UnixTime 2022 6 1 17 0 0)
      :category :seminar
      :image-url (str domain.community.event/dummy-image-base-url "/id/1078/{width}/{height}.jpg")}
     {:id "b8a92f94-66c4-472c-a0ea-7addf8b792ce"
@@ -413,7 +418,7 @@
      :owned-member-id "f94b2700-0507-41bc-9bc7-92aaad30bd7a"
      :name "renovate との戦い"
      :details "適切な運用をして長期的なプロダクトを維持していくための秘訣、教えます"
-     :hold-at (.getTime (js/Date. (js/Date.UTC 2022 6 2 10 0 0)))
+     :hold-at (->UnixTime 2022 6 2 10 0 0)
      :category :seminar
      :image-url (str domain.community.event/dummy-image-base-url "/id/1010/{width}/{height}.jpg")}
     ;; 飲料からモチベを上げ隊
@@ -422,7 +427,7 @@
      :owned-member-id "dff1c891-4d23-4175-a3fe-64c3bac642ca"
      :name "実はカフェインの高いドリンク10選"
      :details "毎日コーヒーとエナジードリンクで飽きたら、こんな選択肢が"
-     :hold-at (.getTime (js/Date. (js/Date.UTC 2022 5 9 16 0 0)))
+     :hold-at (->UnixTime 2022 5 9 16 0 0)
      :category :seminar
      :image-url (str domain.community.event/dummy-image-base-url "/id/1060/{width}/{height}.jpg")}
     {:id "78f07645-1537-421a-a448-fca07a8d664d"
@@ -430,7 +435,7 @@
      :owned-member-id "dff1c891-4d23-4175-a3fe-64c3bac642ca"
      :name "深夜稼働を生き残るための完全栄養ドリンク試飲会"
      :details "限界のその先へ"
-     :hold-at (.getTime (js/Date. (js/Date.UTC 2022 5 10 13 30 0)))
+     :hold-at (->UnixTime 2022 5 10 13 30 0)
      :category :party
      :image-url (str domain.community.event/dummy-image-base-url "/id/1060/{width}/{height}.jpg")}
     ;; 時短飯研究同好会
@@ -440,7 +445,7 @@
      :owned-member-id "fdfd8eaf-2481-41cb-a40b-24871435b58c"
      :name "AutoEncoder で遊ぶ潜在表現操作"
      :details "ディープラーニングで 0 を 1 に書き換えてみる"
-     :hold-at (.getTime (js/Date. (js/Date.UTC 2022 5 11 16 30 0)))
+     :hold-at (->UnixTime 2022 5 11 16 30 0)
      :category :seminar
      :image-url (str domain.community.event/dummy-image-base-url "/id/896/{width}/{height}.jpg")}
     {:id "509f5497-b85d-44ed-835e-59934d290ab7"
@@ -448,7 +453,7 @@
      :owned-member-id "e1c9d298-0d03-4e75-b818-f405ce679d41"
      :name "GANs で描く UMA"
      :details "説得力のある画像の生成を生成する理論的なお話"
-     :hold-at (.getTime (js/Date. (js/Date.UTC 2022 5 12 16 30 0)))
+     :hold-at (->UnixTime 2022 5 12 16 30 0)
      :category :seminar
      :image-url (str domain.community.event/dummy-image-base-url "/id/998/{width}/{height}.jpg")}
     {:id "68789f57-44a0-4c25-a145-e1dadda2decd"
@@ -456,7 +461,7 @@
      :owned-member-id "fdfd8eaf-2481-41cb-a40b-24871435b58c"
      :name "Flow-based Model で始める可逆ネットワーク"
      :details "可逆変換を駆使して、あっちの世界とこっちの世界を行ったり来たりしてみる"
-     :hold-at (.getTime (js/Date. (js/Date.UTC 2022 5 13 16 30 0)))
+     :hold-at (->UnixTime 2022 5 13 16 30 0)
      :category :seminar
      :image-url (str domain.community.event/dummy-image-base-url "/id/998/{width}/{height}.jpg")}
     ;; 和菓子のいろは
@@ -465,7 +470,7 @@
      :owned-member-id "162dc9f9-1038-4d2e-84ba-e6387d7e4fe0"
      :name "こどもの日を楽しむ持ち寄りパーチー"
      :details "※柏餅の皮は食べれません"
-     :hold-at (.getTime (js/Date. (js/Date.UTC 2022 5 5 11 30 0)))
+     :hold-at (->UnixTime 2022 5 5 11 30 0)
      :category :party
      :image-url (str domain.community.event/dummy-image-base-url "/id/674/{width}/{height}.jpg")}
     {:id "61add728-37d2-485e-b5ca-0d4226fd981c"
@@ -473,7 +478,7 @@
      :owned-member-id "162dc9f9-1038-4d2e-84ba-e6387d7e4fe0"
      :name "わらびもち！！"
      :details "やわらかぷるぷるフレッシュなわらび餅を作る"
-     :hold-at (.getTime (js/Date. (js/Date.UTC 2022 5 15 11 30 0)))
+     :hold-at (->UnixTime 2022 5 15 11 30 0)
      :category :party
      :image-url (str domain.community.event/dummy-image-base-url "/id/998/{width}/{height}.jpg")}
     {:id "34c596a8-7444-4313-855f-e96abc84413e"
@@ -481,7 +486,7 @@
      :owned-member-id "162dc9f9-1038-4d2e-84ba-e6387d7e4fe0"
      :name "求肥から始めるもちもち大福錬成"
      :details "新鮮いちごを添えて"
-     :hold-at (.getTime (js/Date. (js/Date.UTC 2022 5 21 12 30 0)))
+     :hold-at (->UnixTime 2022 5 21 12 30 0)
      :category :party
      :image-url (str domain.community.event/dummy-image-base-url "/id/674/{width}/{height}.jpg")}
     ;; 2022年度版イラストレーション研究会
@@ -490,7 +495,7 @@
      :owned-member-id "83652fe0-9a01-44b8-982d-4abd0083b957"
      :name "プロダクトから見る、イラストの情報量"
      :details "プロダクトテーマと画風の活用法を学ぶ会"
-     :hold-at (.getTime (js/Date. (js/Date.UTC 2022 5 18 16 30 0)))
+     :hold-at (->UnixTime 2022 5 18 16 30 0)
      :category :seminar
      :image-url (str domain.community.event/dummy-image-base-url "/id/82/{width}/{height}.jpg")}
     {:id "94acd1dd-e045-41aa-8342-511601baa962"
@@ -498,7 +503,7 @@
      :owned-member-id "83652fe0-9a01-44b8-982d-4abd0083b957"
      :name "デバイスをを考えた、イラストのルール"
      :details "誰がどのデバイスで見られるかを意識した筆設定を学ぶ会"
-     :hold-at (.getTime (js/Date. (js/Date.UTC 2022 5 19 16 30 0)))
+     :hold-at (->UnixTime 2022 5 19 16 30 0)
      :category :seminar
      :image-url (str domain.community.event/dummy-image-base-url "/id/82/{width}/{height}.jpg")}
     {:id "badc3c95-f000-4cd4-b1cd-5cda4edb283e"
@@ -506,7 +511,7 @@
      :owned-member-id "83652fe0-9a01-44b8-982d-4abd0083b957"
      :name "ユーザ層を意識した、イラストのルール"
      :details "メッセージを持たせるためのテクニックを学ぶ会"
-     :hold-at (.getTime (js/Date. (js/Date.UTC 2022 5 20 16 30 0)))
+     :hold-at (->UnixTime 2022 5 20 16 30 0)
      :category :seminar
      :image-url (str domain.community.event/dummy-image-base-url "/id/82/{width}/{height}.jpg")}]
    :community-event-comment
